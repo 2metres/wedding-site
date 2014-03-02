ee4f66bb6e29f32d3bdfed7b8de18b504b888c1c
Andrew Carr <andrew@2metres.com>
2014-03-02 23:22:57 +1100

    fixing up some stuff

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.logger('dev'))
 app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compile
+  {
+    src: __dirname + '/public',
+    compile: compile
   }
 ))
 app.use(express.static(__dirname + '/public'))
