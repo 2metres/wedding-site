12fdf2c751f7d6a9f429c1ae2f4b02804a19b5c0
Andrew Carr <andrew@2metres.com>
2014-06-08 12:43:43 +1000

    Clean up stuff from playing around

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-var http = require("http")
-  , express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
+var http = require("http"),
+    express = require('express'),
+    stylus = require('stylus'),
+    nib = require('nib'),
+    jeet = require('jeet'),
+    fontFace  = require('stylus-font-face');
 
 var app = express()
 
 function compile(str, path) {
   return stylus(str)
     .set('filename', path)
+    .use(jeet())
+    .use(fontFace())
     .use(nib());
 }
 
-app.set("port", process.env.PORT || 3000);
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(express.logger('dev'))
+app.set("port", process.env.PORT || 4000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
 app.use(stylus.middleware(
   {
     src: __dirname + '/public',
     compile: compile
   }
-))
-app.use(express.static(__dirname + '/public'))
+));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index',
-  { title : 'Home' }
-  )
-})
+    {
+      title : 'Home'
+    }
+  );
+});
 
 http.createServer(app).listen(app.get("port"), function() {
   return console.log("Express server listening on port " + app.get("port"));
